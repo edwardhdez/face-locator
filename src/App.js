f148d90f966a18b5a55e2e9cf25154e2bc16a44c
edwardhdez <edwardhdez21@gmail.com>
2019-09-06 22:03:16 -0400

    Fix bug when sign out, the app never come back to the initial state

--- src/App.js
+++ src/App.js
@@ -33,25 +33,27 @@ const particlesOptions = {
   }
 }
 
+const initialState = {
+  input: '',
+  imageUrl: '',
+  box: {},
+  route: 'signin',
+  isSignedIn: false,
+  user: {
+    id: '',
+    name: '',
+    email: '',
+    entries: 0,
+    joined: ''
+  }
+
+}
+
 class App extends React.Component {
 
   constructor() {
     super();
-    this.state = {
-      input: '',
-      imageUrl: '',
-      box: {},
-      route: 'signin',
-      isSignedIn: false,
-      user: {
-        id: '',
-        name: '',
-        email: '',
-        entries: 0,
-        joined: ''
-      }
-
-    }
+    this.state = initialState;
   }
 
   loadUser = (data) => {
@@ -107,6 +109,7 @@ class App extends React.Component {
             .then(count => {
               this.setState(Object.assign(this.state.user, { entries: count }))
             })
+            .catch(console.log)
         }
         this.displayFaceBox(this.calculateFaceLocation(response))
       })
@@ -115,7 +118,7 @@ class App extends React.Component {
 
   onRouteChange = (route) => {
     if (route === 'signout') {
-      this.setState({ isSignedIn: false });
+      this.setState(initialState);
     }
     else if (route === 'home') {
       this.setState({ isSignedIn: true });
